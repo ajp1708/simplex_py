03bd70ca3e03975cb86b1e94fae2460c743dc3a3
Botahamec <botahamec@outlook.com>
2022-10-04 18:38:39 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from fractions import Fraction
+from numpy import matrix
+
+
+def identity_matrix(size: int) -> matrix:
+	pass
+
+
+class Tableau:
+	def __init__(self, mat: matrix):
+		pass
+
+
+	def __eq__(self, other: Tableau) -> bool:
+		pass
+
+
+	def __hash__(self) -> int:
+		pass
+
+
+	def __str__(self) -> str:
+		pass
