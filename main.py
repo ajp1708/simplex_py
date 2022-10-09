02c8b6d29003875e6b1e0530f09911822d60859f
--replace-all <--replace-all>
2022-10-09 15:08:04 -0400

    Implemented more

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 from __future__ import annotations
 from fractions import Fraction
 from typing import List, Tuple
-from numpy import matrix
-
-
-def identity_matrix(size: int) -> matrix:
-	pass
+from numpy import ndarray
+import numpy
 
 
 class Tableau:
-	def __init__(self, mat: matrix):
-		pass
-
-
-	def __eq__(self, other: Tableau) -> bool:
-		pass
-
-
-	def __hash__(self) -> int:
-		pass
+	def __init__(self, matrix: ndarray):
+		self.left = matrix
+		right = numpy.identity(matrix.shape[0])
+		self.right = numpy.vectorize(Fraction)(right)
+		negs = numpy.array([Fraction(-1)] * matrix.shape[1])
+		zeros = numpy.array([Fraction(0)] * matrix.shape[0])
+		self.bottom = numpy.concatenate([negs, zeros])
+		self.side = numpy.array([Fraction(1)] * matrix.shape[0])
 
 
 	def __str__(self) -> str:
@@ -35,17 +30,20 @@ class Tableau:
 
 
 	def done(self) -> bool:
-		pass
+		return self.bottom.min() >= 0
 
 
 	def row_strategy(self) -> List[Fraction]:
-		pass
+		v = self.value(0)
+		f = lambda x: x / v
+		return f(self.side)
 
 
 	def column_strategy(self) -> List[Fraction]:
-		pass
+		v = self.value(0)
+		f = lambda x: x / v
+		return f(self.bottom[self.left.shape[0]:])
 
 
 	def value(self, k: int) -> Fraction:
-		pass
-
+		return self.bottom[self.bottom.size - 1] - k
