980d712a3195f8e9229f6aaee9fad7d4c8acf05e
--replace-all <--replace-all>
2022-10-11 21:23:46 -0400

    Added comments

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ import numpy
 
 class Tableau:
 	def __init__(self, matrix: ndarray):
+		# the left matrix, which was passed into this function
 		self.left = matrix
+		# the right-side matrix, which is an identity matrix
+		# vectorize is needed to fill the matrix with fractions
 		right = numpy.identity(matrix.shape[0])
 		self.right = numpy.vectorize(Fraction)(right)
+		# the bottom row, which is initialized with -1's and zeros
 		negs = numpy.array([Fraction(-1)] * matrix.shape[1])
 		zeros = numpy.array([Fraction(0)] * (matrix.shape[0] + 1))
 		self.bottom = numpy.concatenate([negs, zeros])
+		# The right-side of the tableau, filled with ones
 		self.side = numpy.array([Fraction(1)] * matrix.shape[0])
 
 
@@ -21,6 +26,7 @@ class Tableau:
 
 
 	def _get_row(self, row: int) -> List[Fraction]:
+		#construct a row from the tableau
 		if row < self.left.shape[0]:
 			lst = []
 			for i in range(self.left.shape[1]):
@@ -34,6 +40,7 @@ class Tableau:
 
 
 	def _get_col(self, column: int) -> List[Fraction]:
+		# construct a column from the tableau
 		if column < self.left.shape[1]:
 			lst = []
 			for i in range(self.left.shape[0]):
@@ -54,6 +61,7 @@ class Tableau:
 
 	def pivot(self) -> Tuple[int, int]:
 		# return the location of the pivot in the table
+		# first we get the column number of the pivot
 		min_value = Fraction(0)
 		min_index = 0
 		for i in range(self.bottom.size):
@@ -61,6 +69,7 @@ class Tableau:
 				min_value = self.bottom[i]
 				min_index = i
 
+		# then we get the row number of the pivot
 		col = self._get_col(min_index)
 		max_value = Fraction(10_000_000)
 		max_index = -1
@@ -71,6 +80,7 @@ class Tableau:
 			elif self.side[i] / col[i] < max_value:
 				max_value = self.side[i] / col[i]
 				max_index = i
+		
 		return (min_index, max_index)
 
 
@@ -79,21 +89,25 @@ class Tableau:
 
 
 	def done(self) -> bool:
+		# checks to see if we're done simplexing yet
 		return self.bottom.min() >= 0
 
 
 	def row_strategy(self) -> List[Fraction]:
+		# get the row player's optimal mixed strategy
 		v = self.value(0)
 		f = lambda x: x / v
 		return f(self.side)
 
 
 	def column_strategy(self) -> List[Fraction]:
+		# get the column player's optimal mixed strategy
 		v = self.value(0)
 		f = lambda x: x / v
 		return f(self.bottom[self.left.shape[0]:])
 
 
 	def value(self, k: int) -> Fraction:
+		# the value of the game, assuming we're done
 		return self.bottom[self.bottom.size - 1] - k
 
